7900374e05cfbc4c57c163f8c209a1ad21ec48cc
Michael McCarry <fastegg@gmail.com>
2018-05-26 18:20:24 -0700

    Foundation project, and publish steps

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var git = require('gulp-git');
 var $    = require('gulp-load-plugins')();
 var tsc  = require('gulp-typescript');
 var tsProject = tsc.createProject('tsconfig.json');
 var webpack = require('webpack');
 var config = require('./webpack.config');
+var fs = require('fs');
 
 var sassPaths = [
   'node_modules/foundation-sites/scss',
@@ -44,5 +46,30 @@ gulp.task('webpack', ['typescript'], function(callback) {
 gulp.task('default', ['sass', 'typescript', 'webpack'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
   gulp.watch(['clientjs/**/*'], ['typescript', 'webpack']);
-  //gulp.watch(['build/clientjs/app.js'], ['webpack']);
+});
+
+gulp.task('cloneRepo', function(callback) {
+  console.log('clone!');
+  if (fs.existsSync('./publish')) {
+    git.pull('origin', 'master', callback);
+  } else {
+    git.clone('git@github.com:fastegg/fastegg.github.io.git', {args: './publish'}, callback);
+  }
+});
+
+gulp.task('copyChanges', ['sass', 'typescript', 'webpack', 'cloneRepo'], function() {
+  return gulp.src('./public/**/*').pipe(gulp.dest('./publish'));
+});
+
+gulp.task('commitChanges', ['webpack', 'copyChanges'], function() {
+  return gulp.src('./publish')
+    .pipe(git.add({args: '-A', cwd: './publish'}))
+    .pipe(git.commit('new version', {cwd: './publish'}));
+})
+gulp.task('pushChanges', ['commitChanges'], function() {
+  return git.push('origin', 'master', {cwd: './publish'});
+});
+
+gulp.task('publish', ['sass', 'typescript', 'webpack', 'pushChanges'], function() {
+  console.log('done!');
 });
